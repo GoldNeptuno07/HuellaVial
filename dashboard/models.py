be61ab30552a9269cb635189423fe537c70ecc5a
Angel Cruz <angcruz8333@gmail.com>
2025-03-31 17:07:18 -0600

    Adding minimal changes

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class projects(models.Model):
+    name = models.CharField(max_length=50)
+    creation_date = models.DateField(auto_now_add=True)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def main_view(request):
-    return render(request, "dashboard/dashboard.html", {})
+    # Get the projects
+    projects = models.projects.objects.all()
+    context = {
+        "projects": projects
+    }
+    return render(request, "dashboard/dashboard.html", context)
