d0c0931b722943946bc59d76675783e4156a6c60
Angel Linux <angcruz8333@gmail.com>
2025-03-31 20:59:01 -0600

    Adding project's layout and style

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class projects(models.Model):
     name = models.CharField(max_length=50)
-    creation_date = models.DateField(auto_now_add=True)
+    creation_date = models.DateField(auto_now_add=False)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_list_or_404
+from django.utils import timezone
+from datetime import timedelta
 from . import models
 
 # Create your views here.
 def main_view(request):
     # Get the projects
-    projects = get_list_or_404(models.projects)
-    #projects = models.projects.objects.all()
+    projects = models.projects.objects.all()
+    # Get the most recent projects (projects bewteen 8 days ago and the current time)
+    time = timezone.now() - timedelta(days= 16)
+    recent_projects = projects.filter(creation_date__gte= time).order_by("-creation_date")
+    old_projects= projects.filter(creation_date__lte= time - timedelta(days= 1)).order_by("-creation_date")
     context = {
-        "projects": projects
+        "recent_projects": recent_projects,
+        "old_projects": old_projects,
     }
     return render(request, "dashboard/dashboard.html", context)
