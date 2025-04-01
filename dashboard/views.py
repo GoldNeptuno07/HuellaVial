1cc8af1822eb91c03279354125a995faf2fc269c
Angel Linux <angcruz8333@gmail.com>
2025-03-31 18:44:35 -0600

    Changes in the dashboard layout

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from . import models
 
 # Create your views here.
 def main_view(request):
     # Get the projects
-    projects = models.projects.objects.all()
+    projects = get_list_or_404(models.projects)
+    #projects = models.projects.objects.all()
     context = {
         "projects": projects
     }
