f67e923c3bb32f42e669c06bdc40e4cc5c3dba01
Angel Cruz <angcruz8333@gmail.com>
2025-05-15 19:49:44 -0600

    Adding env variables & SSL certificates to the web

--- HuellaVial/settings.py
+++ HuellaVial/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = "HuellaVial.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "huellavial_db",
-        "USER": "angelcruz",
-        "PASSWORD": "7deenerode2004",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": "db",
         "PORT": 5432,
     }
